4d67df8ca575a4d5bb2bcd4dcc85caa3b2601c5d
graceniyigena34 <graceniyigena34@gmail.com>
2025-11-05 14:02:21 +0200

    I created the folder state that contain the file of the actions .  where we add payment, add client ,add project

---
+++ freelance-dashboard/src/state/actions.ts
@@ -0,0 +1,9 @@
+import { Client, Project, Payment } from "../types";
+
+
+export type Action =
+| { type: "ADD_PAYMENT"; payload: { payment: Payment } }
+| { type: "MARK_PROJECT_PAID"; payload: { projectId: string } }
+| { type: "ADD_CLIENT"; payload: { client: Client } }
+| { type: "ADD_PROJECT"; payload: { project: Project } }
+| { type: "UPDATE_PROJECT_STATUS"; payload: { projectId: string; status: Project["status"] } };
